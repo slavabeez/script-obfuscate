7234baeb8fcf08b1e73bccd8d6069e30f088c36f
slavabeez <slava_berdugin119@mail.ru>
2025-08-18 13:11:14 +0300

    Update check.lua

--- check.lua
+++ check.lua
@@ -1,4 +1,4 @@
-local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/slavabeez/script/refs/heads/main/white%20list.lua", true))()
+local whitelistecheck = loadstring(game:HttpGet("https://raw.githubusercontent.com/slavabeez/script-obfuscate/refs/heads/main/white-list.lua", true))()
 
 if whitelistecheck[game:service('Players').LocalPlayer.UserId] then
   print("sucses") -- ÐÐ°Ñ ÑÐºÑÐ¸Ð¿Ñ
