3953e181a621eac73e8fa0fe4deb936198ee8615
slavabeez <slava_berdugin119@mail.ru>
2026-01-24 13:50:17 +0300

    Update white-list.lua

--- white-list.lua
+++ white-list.lua
@@ -33,6 +33,7 @@ userids = {
 [3378659261] = true, --zombinoobboom
 [1565191893] = true, --Ameclick
 [2420334865] = true, --frorepelitta
+[2652270994] = true, --kolasbyst
 [8130758086] = true,
 [1321436387] = true,
 [2293173453] = true,
